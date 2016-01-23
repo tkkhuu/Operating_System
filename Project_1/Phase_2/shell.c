015f801ff71afcb38d5912601f385617e5a56f1a
Tri Khuu <khatrikhuu@yahoo.com>
2016-01-23 18:07:29 -0500

    shell.c able to read from stdin

--- Project_1/Phase_2/shell.c
+++ Project_1/Phase_2/shell.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
 char* get_command(char* input_string);
 
 int main(int argc, char* argv[]){
@@ -13,13 +13,14 @@ int main(int argc, char* argv[]){
 		// Wait and read for user inputs
 		char user_input[128];
 		fgets(user_input, sizeof(user_input), stdin);
-		
+
+		// Parse the input
 		char* shell_input[32];
 		char* token;
 		char* delim = " \n";
 
 		token = strtok(user_input, delim);
-		
+
 		int input_counts = 0;
 		while(token != NULL){
 			shell_input[input_counts] = token;
@@ -28,54 +29,52 @@ int main(int argc, char* argv[]){
 		}
 
 		shell_input[input_counts] = NULL;
-		int j;
-		for(j = 0; j < input_counts + 1; j++){
-			printf("shell_input[%d]: %s\n", j, shell_input[j]);
+
+		// If the built in function chdir "cd: is called		
+		if (strcmp(shell_input[0], "cd") == 0){
+			if(input_counts > 3){
+				perror("shell: cd error: ");
+			}
+			else{
+				int cd_status = chdir(shell_input[1]);
+				if (cd_status < 0){
+					perror("shell: cd error: ");
+				}
+			}
 		}
-						
+
+		// If the built in command exit is called
+		else if(strcmp(shell_input[0], "exit") == 0){
+			if (input_counts > 1){
+				printf("exit error: Too many arguments\n");
+			}
+			else{
+				printf("Exit shell\n");
+				exit(EXIT_SUCCESS);
+			}
+		}
+
 		// ================ Create a child process ==================
 		// Fork a child process
-		pid_t child_process = fork();
-	
-		if (child_process < 0){ // Create Child process failed !
-			perror("Child process creation failed\n");
-			exit(-1);
-		}
+		else{
+			pid_t child_process = fork();
 
-		if (child_process != 0){ // In the Parent process
-			wait();		
-		}
-
-		if (child_process == 0){ // In the Child process
-			// If the built in command chdir() "cd" is called
-			if (strcmp(shell_input[0], "cd") == 0){
-				if(input_counts > 3){
-					perror("cd error: Too many arguments\n");
-				}
-				else{
-					int cd_statu = chdir(shell_input[1]);
-					if (cd_statu < 0){
-						perror("cd failed\n");
-					}
-				}
+			if (child_process < 0){ // Create Child process failed !
+				perror("Child process creation failed\n");
+				exit(-1);
 			}
 
-			// If the built in command exit is called
-			else if(strcmp(shell_input[0], "exit") == 0){
-				if (input_counts > 2){
-					perror("exit error: Too many arguments\n");
-				}
-				else{
-					exit(0);
-				}
+			if (child_process != 0){ // In the Parent process
+				wait();
 			}
-			
-			// If the other Linux commands are called
-			else if(execvp(shell_input[0], shell_input) < 0){
-				perror("Exec failed: invalid command or arguments\n");
-			} 
-		}
 
+			if (child_process == 0){ // In the Child process
+				// If the other Linux commands are called
+				if(execvp(shell_input[0], shell_input) < 0){
+					perror("Exec failed: invalid command or arguments\n");
+				} 
+			}
+		}
 	}
 }
 
