8dd6230a76b03d31db591e097469d623ff4cd09c
Tri Khuu <khatrikhuu@yahoo.com>
2016-01-24 01:31:59 -0500

    phase 1 and 2 done without statistic

--- Project_1/Phase_2/shell.c
+++ Project_1/Phase_2/shell.c
@@ -2,23 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-char* get_command(char* input_string);
+
+void read_stdin();
 
 int main(int argc, char* argv[]){
 
+	if(argc == 1){
+		read_stdin();
+	}
+	else {
+		printf("Invalid command, usage:\n");
+		printf("1) To open a shell: ./shell\n");
+		printf("2) To read commands from a .txt file: ./shell < file.txt\n");
+	}
+}
+
+void read_stdin(){
 	while(1){
 		// Prompt the user to enter command inputs
 		fputs("==> ", stdout);
 
 		// Wait and read for user inputs
 		char user_input[128];
-		fgets(user_input, sizeof(user_input), stdin);
-
+		char* f_gets_status = fgets(user_input, sizeof(user_input), stdin);
+		if (f_gets_status == NULL){
+			exit(0);
+}
 		// Parse the input
 		char* shell_input[32];
 		char* token;
 		char* delim = " \n";
-
+		
 		token = strtok(user_input, delim);
 
 		int input_counts = 0;
@@ -27,11 +41,14 @@ int main(int argc, char* argv[]){
 			token = strtok(NULL, delim);
 			input_counts++;
 		}
-
+		
 		shell_input[input_counts] = NULL;
-
-		// If the built in function chdir "cd: is called		
-		if (strcmp(shell_input[0], "cd") == 0){
+		
+		// If the built in function chdir "cd: is called
+		if(shell_input[0] == NULL){
+			continue;
+		}		
+		else if (strcmp(shell_input[0], "cd") == 0){
 			if(input_counts > 3){
 				perror("shell: cd error: ");
 			}
@@ -67,12 +84,14 @@ int main(int argc, char* argv[]){
 			if (child_process != 0){ // In the Parent process
 				wait();
 			}
-
+			
 			if (child_process == 0){ // In the Child process
 				// If the other Linux commands are called
+				
 				if(execvp(shell_input[0], shell_input) < 0){
 					perror("Exec failed: invalid command or arguments\n");
 				} 
+				
 			}
 		}
 	}
