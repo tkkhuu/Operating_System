7859ebb1a93b3326508ffed86b2441d7c12fbae8
Tri Khuu <khatrikhuu@gmail.com>
2016-02-10 23:02:00 -0500

    semaphore method working, deadlock occurs

--- Project_3/chef.c
+++ Project_3/chef.c
@@ -9,7 +9,6 @@
 #define CHEF_NUM 3
 #define TRUE 1
 
-
 #define PREP    0
 #define STOVE   1
 #define OVEN    2
@@ -58,17 +57,20 @@ struct recipe_struct {
 typedef struct recipe_struct recipe;
 
 void chef(int *chef_id);
-void enter_station(int *chef_id, recipe *current_recipe);
-void leave_station(int *chef_id, recipe *current_recipe);
-void perform_step (int *chef_id, recipe *current_recipe);
+void enter_station(int *chef_id, recipe *current_recipe, int order_number);
+void leave_station(int *chef_id, recipe *current_recipe, int order_number);
+void perform_step (int *chef_id, recipe *current_recipe, int order_number);
+char *get_station_name(int station_id);
 
 int current_order = 0;
+
+int chef_state[3] = {-1, -1, -1};
 
 sem_t kitchen[4];
 
 sem_t recipe_mutex[5];
 
-
+sem_t state_mutex;
 
 sem_t lor_mutex;
 
@@ -222,43 +224,57 @@ recipe oders[N];
 /**
  * Function to enter the stations, which is the critical regions.
  */
-void enter_station(int *chef_id, recipe *current_recipe){
+void enter_station(int *chef_id, recipe *current_recipe, int order_number){
 
 	int step_to_perform = current_recipe->steps[current_recipe->next_action].action; // Next step to be performed: will either be PREP, STOVE, OVEN or SINK
 
-	printf("Chef %d is waiting to enter station %d\n", *chef_id, step_to_perform);	
+	sem_wait(&state_mutex);
+
+	printf("Chef %d is in %s, waiting to enter station %s, to work on order %d, recipe %d, step %d\n", *chef_id, get_station_name(chef_state[*chef_id - 1]), get_station_name(step_to_perform), current_recipe-> recipe_type, order_number, step_to_perform);	
 	
-	sem_wait(&kitchen[step_to_perform]); // If the part of this kitchen is being used, sleep
+	sem_wait(&kitchen[step_to_perform]); // If the part of this kitchen is being used, sleep	
+
+	if (chef_state[*chef_id - 1] != -1) {
+		sem_post(&kitchen[chef_state[*chef_id - 1]]);
+	}
+
+	chef_state[*chef_id - 1] = step_to_perform;
+
+	printf("Chef 1 is in: %s, chef 2 is in %s, chef 3 is in %s\n", get_station_name(chef_state[0]), get_station_name(chef_state[1]), get_station_name(chef_state[2]));
+
+	sem_post(&state_mutex);
 
 }
 
 /**
  * Function to leave the critical region
  */
-void leave_station(int *chef_id, recipe *current_recipe){
+void leave_station(int *chef_id, recipe *current_recipe, int order_number){
 
 	int step_finished = current_recipe->steps[current_recipe->next_action].action; // Get the step that the chef just finished
 
-	printf("Chef %d is leaving station %d\n", *chef_id, step_finished);
+	printf("Chef %d finished step %s of order %d, is leaving station %s\n", *chef_id, get_station_name(step_finished), order_number, get_station_name(step_finished));
 
 	// Move to next step on the recipe steps, if no more step, mark the recipe as finished.
 	if (current_recipe->next_action == current_recipe->num_action) {
 		current_recipe->is_done = 1;
+		printf("Order %d is finished by chef %d\n", order_number, *chef_id);
 	} else {
 		current_recipe->next_action++;
 	}
+	
+	// printf("Chef %d needs to leave station %s\n", *chef_id, get_station_name(step_finished));
 
-	// Release the semaphore for the station.
-	sem_post(&kitchen[step_finished]);
-
-	printf("Chef %d left station %d\n", *chef_id, step_finished);
+	
 }
 
 /**
  * Function to perform the step
  * Takes in a recipe
  */
-void perform_step (int *chef_id, recipe *current_recipe) {
+void perform_step (int *chef_id, recipe *current_recipe, int order_number) {
+
+	int curr_station = current_recipe->steps[current_recipe->next_action].action;
 
 	int time_taken = current_recipe->steps[current_recipe->next_action].time_period;
 
@@ -268,7 +284,7 @@ void perform_step (int *chef_id, recipe *current_recipe) {
 
 	gettimeofday(&start, NULL);
 
-	printf("Chef %d is working in station %d\n", *chef_id, current_recipe->steps[current_recipe->next_action].action);
+	printf("Chef %d is working in station %s\n", *chef_id, get_station_name(curr_station));
 
 	while ( time_elapsed < time_taken) {
 
@@ -292,7 +308,7 @@ char *get_station_name(int station_id){
             break;
             
         case 2:
-            return "Oven"
+            return "Oven";
             break;
             
         case 3:
@@ -300,6 +316,7 @@ char *get_station_name(int station_id){
             break;
             
         default:
+				return "Wait Room";
             break;
     }
 }
@@ -313,6 +330,8 @@ void chef(int *chef_id){
 
 	recipe *current_recipe = NULL;
 
+	int order_num = 0;
+
 	while (TRUE) {
 
 		/** If the chef is not working on any order, request an order. */
@@ -324,6 +343,8 @@ void chef(int *chef_id){
 
 			current_recipe = next_order();
 
+			order_num = current_order + 1;
+
 			sem_post(&lor_mutex);
 
 			printf("Chef %d got recipe %d\n", *chef_id, current_recipe->recipe_type);
@@ -334,19 +355,21 @@ void chef(int *chef_id){
 
 				printf("Chef %d is working on recipe %d\n", *chef_id, current_recipe->recipe_type);
 
-				sem_wait(&recipe_mutex[current_recipe->recipe_type - 1]);
+				//sem_wait(&recipe_mutex[current_recipe->recipe_type - 1]);
 
-				enter_station(chef_id, current_recipe);
+				enter_station(chef_id, current_recipe, order_num);
 
-				perform_step(chef_id, current_recipe);
+				perform_step(chef_id, current_recipe, order_num);
 
-				leave_station(chef_id, current_recipe);
+				leave_station(chef_id, current_recipe, order_num);
+
+				//sem_post(&recipe_mutex[current_recipe->recipe_type - 1]);
 
 			} else if (current_recipe->is_done == 1){
 
-				sem_post(&recipe_mutex[current_recipe->recipe_type - 1]);
+				//sem_post(&recipe_mutex[current_recipe->recipe_type - 1]);
 
-				printf("Chef %d is finished recipe %d\n", *chef_id, current_recipe->recipe_type);
+				printf(" ================ Chef %d finished order %d recipe %d ================\n", *chef_id, order_num, current_recipe->recipe_type);
 
 				current_recipe = NULL;
 
@@ -373,6 +396,8 @@ int main (int argc, char* argv[]){
 	printf("Initializing orders semaphore ...\n");
 
 	sem_init(&lor_mutex, 0, 1);
+
+	sem_init(&state_mutex, 0, 1);
 
 	printf("Initializing kitchen semaphores ...\n");
 
